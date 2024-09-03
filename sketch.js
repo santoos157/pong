dcc5deae840c00a6374175f4e4b6fefaf9176558
santoos157 <leticia.gomes.santos22@escola.pr.gov.br>
2024-09-03 09:02:14 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -136,8 +136,8 @@ function marcaPonto() {
 
 
 function preload(){
-  trilha = loadSound("trilha.mp3");
-  ponto = loadSound("ponto.mp3");
-  raquetada = loadSound("raquetada.mp3");
+  trilha = loadSound("songs/trilha.mp3");
+  ponto = loadSound("songs/ponto.mp3");
+  raquetada = loadSound("songs/raquetada.mp3");
 }
 
